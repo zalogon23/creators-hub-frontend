c28cbeef3c6d61112705357f6906dcce9c986865
gzarate <gzarate@HXNB-1967>
2023-08-20 20:06:13 -0500

    Media Player fix for mobile.

--- src/components/MediaPlayer.tsx
+++ src/components/MediaPlayer.tsx
@@ -1,6 +1,6 @@
 import { faExpand, faPause, faPlay } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { MouseEventHandler, createRef, useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 type Props = {
     url: string,
@@ -11,6 +11,7 @@ function MediaPlayer({ url, thumbnail }: Props) {
     const [paused, setPaused] = useState(true);
     const [interacted, setInteracted] = useState(false);
     const [hovered, setHovered] = useState(false);
+    const [myTimeout, setMyTimeout] = useState(null as any as NodeJS.Timeout)
 
     const mediaRef = useRef<HTMLElement>(null);
     const videoRef = useRef<HTMLVideoElement>(null);
@@ -62,14 +63,18 @@ function MediaPlayer({ url, thumbnail }: Props) {
             }
         };
 
-        document.addEventListener("keydown", handleSpacebar);
-        document.addEventListener("mousemove", handleMouseMove);
+        document.addEventListener("keydown", handleSpacebar)
+        document.addEventListener("mousemove", handleMouseMove)
         document.addEventListener("mouseup", handleMouseUp)
+        document.addEventListener("touchmove", handleMouseMove)
+        document.addEventListener("touchend", handleMouseUp)
 
         return () => {
             document.removeEventListener("keydown", handleSpacebar);
-            document.removeEventListener("mousemove", handleMouseMove);
+            document.removeEventListener("mousemove", handleMouseMove)
             document.removeEventListener("mouseup", handleMouseUp)
+            document.addEventListener("touchmove", handleMouseMove)
+            document.addEventListener("touchend", handleMouseUp)
         };
     }
 
@@ -99,16 +104,22 @@ function MediaPlayer({ url, thumbnail }: Props) {
         setupEventListeners()
     }, []);
 
-    const getTimeRatio = (event: MouseEvent) => {
+    const getTimeRatio = (event: MouseEvent | TouchEvent) => {
         const barWidth = mediaRef.current!.offsetWidth;
-        const mouseX = event.pageX - mediaRef.current!.offsetLeft;
+        let mouseX: number
+        if (event instanceof MouseEvent) {
+            mouseX = event.pageX - mediaRef.current!.offsetLeft;
+        } else {
+            var touch = event.touches[0] || event.changedTouches[0]
+            mouseX = touch.pageX
+        }
         let ratio = (mouseX / barWidth);
         if (ratio > 1) ratio = 1
         if (ratio < 0) ratio = 0
         return ratio
     }
 
-    const handleMouseUp = (event: MouseEvent) => {
+    const handleMouseUp = (event: MouseEvent | TouchEvent) => {
         if (!mediaRef.current || !barPosition.current || !draggingPlayerBarRef.current) return
         barPosition.current.style.width = "0%";
         isMouseDownRef.current = false;
@@ -121,7 +132,8 @@ function MediaPlayer({ url, thumbnail }: Props) {
         }
     }
 
-    const handleMouseMove = (event: MouseEvent) => {
+    const handleMouseMove = (event: MouseEvent | TouchEvent) => {
+        console.log(draggingPlayerBarRef.current)
         if (!mediaRef.current) return
         const mediaPlayerBarOnScreen = (pausedRef.current && interactedRef.current) || (hoveredRef.current && !pausedRef.current)
         const onBar = event.target == bar.current
@@ -136,13 +148,13 @@ function MediaPlayer({ url, thumbnail }: Props) {
         }
     };
 
-    const handleMouseDown: MouseEventHandler<HTMLDivElement> = (event) => {
+    const handleMouseDown = (event: TouchEvent | MouseEvent) => {
         isMouseDownRef.current = true
         draggingPlayerBarRef.current = true
         bar.current?.classList.add("dragging")
         videoRef.current!.pause()
         wasPausedRef.current = pausedRef.current
-        handleMouseMove(event as any as MouseEvent)
+        handleMouseMove(event)
     }
 
     useEffect(() => {
@@ -158,48 +170,91 @@ function MediaPlayer({ url, thumbnail }: Props) {
         }
     }, [paused]);
 
+    const isMobile = () => {
+        if (navigator.userAgent.match(/Android/i)
+            || navigator.userAgent.match(/webOS/i)
+            || navigator.userAgent.match(/iPhone/i)
+            || navigator.userAgent.match(/iPad/i)
+            || navigator.userAgent.match(/iPod/i)
+            || navigator.userAgent.match(/BlackBerry/i)
+            || navigator.userAgent.match(/Windows Phone/i)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    const hide = () => {
+        setHovered(false)
+    }
+
     return (
         <div className="cont">
             <section
                 ref={mediaRef}
                 onMouseEnter={() => {
-                    setHovered(true)
-                    hoveredRef.current = true
+                    if (!isMobile()) {
+                        setHovered(true)
+                    }
                 }}
                 onMouseLeave={() => {
-                    setHovered(false)
-                    hoveredRef.current = false
+                    if (!isMobile()) {
+                        setHovered(false)
+                    }
                 }}
                 className="relative media-player"
             >
                 <video
+                    playsInline
                     onEnded={() => setPaused(true)}
                     ref={videoRef}
                     autoPlay
-                    onClick={toggle}
+                    onClick={(e) => {
+                        e.preventDefault()
+                        console.log(myTimeout)
+                        if (isMobile()) {
+                            setHovered(h => !h)
+                            if (myTimeout) clearTimeout(myTimeout)
+                            setMyTimeout(setTimeout(hide, 1800))
+                        } else {
+                            toggle()
+                        }
+                    }}
                     poster={thumbnail}
                     src={url}
                 >
                 </video>
-                <div className={`play alert ${!paused && "active-alert"}`}>
+                <div className={`play alert ${!paused && !isMobile() && "active-alert"}`}>
                     <FontAwesomeIcon icon={faPlay} />
                 </div>
-                <div className={`pause alert ${interacted && paused && "active-alert"}`}>
+                <div className={`pause alert ${interacted && !isMobile() && paused && "active-alert"}`}>
                     <FontAwesomeIcon icon={faPause} />
                 </div>
+                <div
+                    onClick={() => { play(); console.log("play button") }}
+                    className={`play alert ${paused && isMobile() && "fixed-alert"}`}>
+                    <FontAwesomeIcon icon={faPlay} />
+                </div>
+                <div
+                    onClick={() => pause()}
+                    className={`pause alert ${interacted && hovered && isMobile() && !paused && "fixed-alert"}`}>
+                    <FontAwesomeIcon icon={faPause} />
+                </div>
+
                 {
                     interacted
                     &&
                     <section
                         className={`${(paused && interacted) || (hovered && !paused) || draggingPlayerBarRef.current
                             ? "opacity-1"
-                            : "opacity-0"
+                            : "opacity-0 pointer-events-none"
                             } bg-black/50 text-white w-full p-3 px-5 duration-150 absolute text-xl bottom-0 controls`}
                     >
                         <div
                             className="loading-bar"
                             ref={bar}
-                            onMouseDown={handleMouseDown}
+                            onMouseDown={(e) => handleMouseDown(e.nativeEvent)}
+                            onTouchStart={(e) => handleMouseDown(e.nativeEvent)}
                             onMouseLeave={() => {
                                 barPosition.current!.style.width = "0%"
                             }}
